d991efacc3c2036ff7dfafd1aa716f9871a2d69b
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-11 17:24:18 -0300

    solved the first part of the first exercise of the second module of OOP and Java

---
+++ Java e POO - 2/exercicio-pratico-1/src/com/company/Pessoa.java
@@ -0,0 +1,21 @@
+package com.company;
+
+//Exercício 1
+
+public class Pessoa {
+    private String nome;
+    private Integer idade;
+    private String id;
+    private float peso;
+    private float altura;
+}
+
+
+public Pessoa(String nome, Integer idade, String id, float peso, float altura){
+    this.nome = nome;
+    this.idade = idade;
+    this.id = id;
+    this.peso = peso;
+    this.altura = altura;
+}
+
