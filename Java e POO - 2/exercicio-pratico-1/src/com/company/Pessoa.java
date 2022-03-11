61b10640f3a6a8f0480dda9d1b7d6833dab8d192
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-11 17:31:15 -0300

    second part of the Pessoa exercise. Adjusted the class structure

--- Java e POO - 2/exercicio-pratico-1/src/com/company/Pessoa.java
+++ Java e POO - 2/exercicio-pratico-1/src/com/company/Pessoa.java
@@ -1,6 +1,6 @@
 package com.company;
 
-//Exercício 1
+// Exercício 1
 
 public class Pessoa {
     private String nome;
@@ -8,14 +8,25 @@ public class Pessoa {
     private String id;
     private float peso;
     private float altura;
+
+
+// Exercício 2
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, Integer idade, String id) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+    }
+
+    public Pessoa(String nome, Integer idade, String id, float peso, float altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
-
-
-public Pessoa(String nome, Integer idade, String id, float peso, float altura){
-    this.nome = nome;
-    this.idade = idade;
-    this.id = id;
-    this.peso = peso;
-    this.altura = altura;
-}
-
