78edda41eda11303f829610975eb180b04d81518
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-11 17:41:32 -0300

    instatiated 3 new person objects and tested the creation of one with a constructor that didn't exsit

--- Java e POO - 2/exercicio-pratico-1/src/com/company/Main.java
+++ Java e POO - 2/exercicio-pratico-1/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+// Exercício 3
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // Exercício 4
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa("Luiz", 26, "2");
+        Pessoa pessoa3 = new Pessoa("Felipe", 26, "2", 76.5, 1.90);
+
+        // won't work, because the constructor differs in length
+        // Pessoa pessoaTeste = new Pessoa("Pessoa teste", 27);
     }
 }
--- Java e POO - 2/exercicio-pratico-1/src/com/company/Pessoa.java
+++ Java e POO - 2/exercicio-pratico-1/src/com/company/Pessoa.java
@@ -6,8 +6,8 @@ public class Pessoa {
     private String nome;
     private Integer idade;
     private String id;
-    private float peso;
-    private float altura;
+    private double peso;
+    private double altura;
 
 
 // Exercício 2
@@ -22,7 +22,7 @@ public class Pessoa {
         this.id = id;
     }
 
-    public Pessoa(String nome, Integer idade, String id, float peso, float altura) {
+    public Pessoa(String nome, Integer idade, String id, double peso, double altura) {
         this.nome = nome;
         this.idade = idade;
         this.id = id;
