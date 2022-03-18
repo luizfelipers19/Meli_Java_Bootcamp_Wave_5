8c2a322a446ffcbba8762b45affdd2d8f5b08ba3
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-18 17:14:05 -0300

    added the new project of Sports

---
+++ Java Spring/Esporte/src/test/java/com/example/esporte/EsporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
