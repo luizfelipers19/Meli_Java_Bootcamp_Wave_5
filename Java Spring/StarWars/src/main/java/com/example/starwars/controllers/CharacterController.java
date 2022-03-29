e63bcf52f2d1cefb7b2f99151276628a00ac3faa
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-03-29 11:20:39 -0300

    adding some unfinished projects to be finished later, and the tests section

--- Java Spring/StarWars/src/main/java/com/example/starwars/controllers/CharacterController.java
+++ Java Spring/StarWars/src/main/java/com/example/starwars/controllers/CharacterController.java
@@ -1,4 +1,10 @@
 package com.example.starwars.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CharacterController {
+
+    private final CharacterService characterService;
+
 }
---
+++ Tests/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
@@ -0,0 +1,16 @@
+package com.meli.obterdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+
+    
+    String studentName;
+    String message;
+    Double averageScore;
+    List<SubjectDTO> subjects;
+}
