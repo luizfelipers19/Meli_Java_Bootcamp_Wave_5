0b11ce63888f3fec256b0da37096db9babe0c057
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-04-18 17:23:30 -0300

    adding the exceptions exercise scaffold

--- Tests/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
+++ Tests/7.1 ObterDiploma (Base)/src/main/java/com/meli/obterdiploma/model/StudentDTO.java
@@ -3,12 +3,13 @@ package com.meli.obterdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
 
-    
+    @NotBlank
     String studentName;
     String message;
     Double averageScore;
